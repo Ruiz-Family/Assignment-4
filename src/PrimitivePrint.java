bb39b35c9be977d9f912b03c21c067ecf71ab069
Kakarot <JamesRuiz@gatech.edu>
2015-01-27 03:47:57 -0500

    Init commit for Assignment 4

---
+++ src/PrimitivePrint.java
@@ -0,0 +1,15 @@
+/*
+ * Designed by: James Ruiz
+ * Solved by: Chavez Ruiz
+ * @Date: January 27, 2015
+ */
+public class PrimitivePrint {
+
+	
+	public static void main(String[] args) {
+		// TODO Print all primitive data types to the screen with valid values
+		// TODO Do this 5 different times
+		// TODO Do a print statement before the list of primitives, stating which batch it is.
+	}
+
+}
