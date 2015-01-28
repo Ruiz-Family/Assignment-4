766574f00d651f64be7372933f1a82dd03648da0
Chavez Ruiz <chavez.ruiz94@gmail.com>
2015-01-27 21:46:05 -0500

    finished assignment

--- src/PrimitivePrint.java
+++ src/PrimitivePrint.java
@@ -10,6 +10,110 @@ public class PrimitivePrint {
 		// TODO Print all primitive data types to the screen with valid values
 		// TODO Do this 5 different times
 		// TODO Do a print statement before the list of primitives, stating which batch it is.
+		System.out.println("FIRST BATCH");
+		
+		
+		int x = 5;
+		double b = 25.2;
+		char chavez = 'd';
+		String str = new String("i love my self");
+		int k = 18;	
+		int ko = 21;	
+		boolean isSmaller = k < ko;
+		
+		System.out.println(isSmaller);
+	    System.out.println(str);	
+		System.out.println(x);
+		System.out.println(b);
+		System.out.println(chavez);
+		
+		
+		
+		
+		System.out.println("SECOND BATCH");
+		
+		int c = 20;
+		double kj = 17;
+		char nexus = '7';
+		String ajr = new String("my life");
+		int no = 88;
+		int xmas = 77;
+		boolean isgreater = no < xmas;
+		
+		System.out.println(c);
+		System.out.println(kj);
+		System.out.println(nexus);
+		System.out.println(ajr);
+		System.out.println(isgreater);
+		
+		
+		
+		
+		System.out.println("THIRD BATCH");
+		
+		
+		
+		
+		int z = 99;
+		double aj = 41;
+		char lexus = 'p';
+		String swag = new String("No Swag");
+		int anna = 27;
+		int mikey =23;
+		boolean isNicer = mikey > anna;
+		
+		
+		
+		
+		
+		
+		System.out.println(isNicer);
+		System.out.println(swag);
+		System.out.println(lexus);
+		System.out.println(aj);
+		System.out.println(z);
+		
+		
+		
+		
+		System.out.println("FOURTH BATCH");
+		int cha = 99;
+		int all = 98;
+		boolean moreSwag = cha > all;
+		String lexington = new String("indians");
+		char camaro = 'q';
+		double akv = 67.8;
+		int i = 123;
+		
+		
+		System.out.println(moreSwag);
+		System.out.println(lexington);
+		System.out.println(camaro);
+		System.out.println(akv);
+		System.out.println(i);
+		
+		
+		System.out.println("5th BATCH");
+		
+		
+		
+		
+		
+		int teen = 113;
+		double alo = 99.10;
+		char jackson = '2';
+		String laughing = new String("Always laughing");
+		int marie = 121;
+		int toni = 121;
+		boolean sistermom = marie > toni;
+		
+		
+		System.out.println(teen);
+		System.out.println(alo);
+		System.out.println(jackson);
+		System.out.println(laughing);
+		System.out.println(sistermom);
+		
 	}
 
 }
